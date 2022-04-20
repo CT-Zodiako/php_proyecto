31a6eb6d008fc55dd70fe74fb8d88909dbd87c21
Zodiako <cristianjvz98@gmail.com>
2022-04-20 09:31:41 -0500

    Se hizo conexion con la base de datos

--- Guardar_usuario.php
+++ Guardar_usuario.php
@@ -1,8 +1,27 @@
 <?php 
-$user_code = $_POST['user_code'];
-$user_cedula = $_POST['user_cedula'];
-$user_name = $_POST['user_name'];
-$user_celular = $_POST['user_celular'];
+include('conexion.php');
+if(isset($_POST['Guardar_usuario']))
+{
+    $user_code = $_POST['user_code'];
+    $user_cedula = $_POST['user_cedula'];
+    $user_name = $_POST['user_name'];
+    $user_celular = $_POST['user_celular'];
+
+    $consulta = "INSERT INTO usuarios(codigo,cedula,nombre,elular) values('$user_code','$user_cedula','$user_name','$user_celular')";
+    $result = mysqli_query($conn, $consulta);
+
+    if(!$result)
+    {
+
+
+        die("ERROR EN EL QUERY");
+
+    }
+    else {
+        header('Location: GestionUsuarios.php ');
+    }
+}
+
 
 echo  $user_code."<br>";
 echo  $user_cedula."<br>";
